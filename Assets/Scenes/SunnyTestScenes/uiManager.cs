6121cf16228a4b0dbe634c1e4fde4f54f57b50e6
Sunny L. <94069216+sunnylin933@users.noreply.github.com>
2024-04-24 02:36:35 -0400

    Migrating Tilemaps to Canvas

--- Assets/Scenes/SunnyTestScenes/uiManager.cs
+++ Assets/Scenes/SunnyTestScenes/uiManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,11 @@ public class uiManager : MonoBehaviour
 {
     private bool isPlayed;
     //Starter Positions
-    Vector3 leftPanelStart;
-    Vector3 rightPanelStart;
-    Vector3 topShutterStart;
-    Vector3 bottomShutterStart;
-    Vector3 deathScreenStart;
+    Vector2 leftPanelStart;
+    Vector2 rightPanelStart;
+    Vector2 topShutterStart;
+    Vector2 bottomShutterStart;
+    Vector2 deathScreenStart;
 
     [Header("GameObject References")]
     public GameObject leftPanel;
@@ -23,43 +24,43 @@ public class uiManager : MonoBehaviour
     public float transitionShutterTime;
     public float transitionDeathTime;
     [Header("Goalposts")]
-    public Vector3 leftPanelGoal;
-    public Vector3 rightPanelGoal;
-    public Vector3 topShutterGoal;
-    public Vector3 bottomShutterGoal;
-    public Vector3 deathScreenGoal;
+    public Vector2 leftPanelGoal;
+    public Vector2 rightPanelGoal;
+    public Vector2 topShutterGoal;
+    public Vector2 bottomShutterGoal;
+    public Vector2 deathScreenGoal;
 
     void Start()
     {
-        leftPanelStart = leftPanel.transform.localPosition;
-        rightPanelStart = rightPanel.transform.localPosition;
-        topShutterStart = topShutter.transform.localPosition;
-        bottomShutterStart = bottomShutter.transform.localPosition;
-        deathScreenStart = deathScreen.transform.localPosition;
+        LeanTween.init(800);
+        leftPanelStart = leftPanel.GetComponent<RectTransform>().anchoredPosition;
+        rightPanelStart = rightPanel.GetComponent<RectTransform>().anchoredPosition;
+        topShutterStart = topShutter.GetComponent<RectTransform>().anchoredPosition;
+        bottomShutterStart = bottomShutter.GetComponent<RectTransform>().anchoredPosition;
+        deathScreenStart = deathScreen.GetComponent<RectTransform>().anchoredPosition;
     }
 
     void Update()
     {
-        LeanTween.init(800);
         //If the player dies
         if (deathScreen.activeSelf && !isPlayed)
         {
             //Hide side panels
-            LeanTween.moveLocal(leftPanel, new Vector3(leftPanelGoal.x, leftPanelGoal.y, leftPanelGoal.z), transitionPanelTime).setEase(LeanTweenType.easeOutCubic);
-            LeanTween.moveLocal(rightPanel, new Vector3(rightPanelGoal.x, rightPanelGoal.y, rightPanelGoal.z), transitionPanelTime).setEase(LeanTweenType.easeOutCubic);
+            LeanTween.move(leftPanel.GetComponent<RectTransform>(), leftPanelGoal, transitionPanelTime).setEase(LeanTweenType.easeOutCubic);
+            LeanTween.move(rightPanel.GetComponent<RectTransform>(), rightPanelGoal, transitionPanelTime).setEase(LeanTweenType.easeOutCubic);
             //Collapse Shutter
-            LeanTween.moveLocal(topShutter, new Vector3(topShutterGoal.x, topShutterGoal.y, topShutterGoal.z), transitionShutterTime).setDelay(0.15f).setEase(LeanTweenType.easeOutSine);
-            LeanTween.moveLocal(bottomShutter, new Vector3(bottomShutterGoal.x, bottomShutterGoal.y, bottomShutterGoal.z), transitionShutterTime).setDelay(0.15f).setEase(LeanTweenType.easeOutSine);
+            LeanTween.move(topShutter.GetComponent<RectTransform>(), topShutterGoal, transitionShutterTime).setDelay(0.15f).setEase(LeanTweenType.easeOutSine);
+            LeanTween.move(bottomShutter.GetComponent<RectTransform>(), bottomShutterGoal, transitionShutterTime).setDelay(0.15f).setEase(LeanTweenType.easeOutSine);
             //Bring in death menu
-            LeanTween.moveLocal(deathScreen, new Vector3(deathScreenGoal.x, deathScreenGoal.y, deathScreenGoal.z), transitionDeathTime).setDelay(0.5f).setEase(LeanTweenType.easeOutBack);
+            LeanTween.move(deathScreen.GetComponent<RectTransform>(), deathScreenGoal, transitionDeathTime).setDelay(0.5f).setEase(LeanTweenType.easeOutBack);
             isPlayed = true;
         }
         else if(!deathScreen.activeSelf && isPlayed){
-            LeanTween.moveLocal(leftPanel, new Vector3(leftPanelStart.x, leftPanelStart.y, leftPanelStart.z), 1f);
-            LeanTween.moveLocal(rightPanel, new Vector3(rightPanelStart.x, rightPanelStart.y, rightPanelStart.z), 1f);
-            LeanTween.moveLocal(topShutter, new Vector3(topShutterStart.x, topShutterStart.y, topShutterStart.z), 1f);
-            LeanTween.moveLocal(bottomShutter, new Vector3(bottomShutterStart.x, bottomShutterStart.y, bottomShutterStart.z), 1f);
-            LeanTween.moveLocal(deathScreen, new Vector3(deathScreenStart.x, deathScreenStart.y, deathScreenStart.z), 1f);
+            LeanTween.move(leftPanel.GetComponent<RectTransform>(), leftPanelStart, 1f);
+            LeanTween.move(rightPanel.GetComponent<RectTransform>(), rightPanelStart, 1f);
+            LeanTween.move(topShutter.GetComponent<RectTransform>(), topShutterStart, 1f);
+            LeanTween.move(bottomShutter.GetComponent<RectTransform>(), bottomShutterStart, 1f);
+            LeanTween.move(deathScreen.GetComponent<RectTransform>(), deathScreenStart, 1f);
 
             isPlayed = false;
         }
